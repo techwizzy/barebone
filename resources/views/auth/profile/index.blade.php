d2b9349078a82318f545d31d1a76f3a6d6fd87c4
techwizzy <mugambialois@gmail.com>
2022-10-16 15:09:35 +0300

    Updated user profile

--- resources/views/auth/profile/index.blade.php
+++ resources/views/auth/profile/index.blade.php
@@ -81,9 +81,34 @@
                     </div>
 
                     <div class="tab-pane" id="settings-b2">
-                        <p>Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt.Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.</p>
-                        <p class="mb-0">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.</p>
-                    </div>
+                        <div class="card">
+                            <div class="table-responsive">
+                                <table class="table table-centered">
+                                    <thead>
+                                        <tr>
+                                            <th colspan="2">Notification Settings</th>
+
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Receive SMS about your account</td>
+                                            <td>
+                                                <input type="checkbox"   data-plugin="switchery" data-color="#64b0f2" data-size="small" />
+                                            </td>
+                                        </tr>
+
+                                        <tr>
+                                            <td>Receive Email alerts </td>
+                                            <td>
+                                                <input type="checkbox"   data-plugin="switchery" data-color="#64b0f2" data-size="small" />
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+
                 </div>
             </div>
         </div>
